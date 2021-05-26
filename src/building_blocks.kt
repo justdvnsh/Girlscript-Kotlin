dbdb0b25214c00b6f1cbf3cc09f80f03a8219cf1
Divyansh Dwivedi <justdvnsh2208@gmail.com>
2021-05-26 21:04:30 +0530

    inital commit

---
+++ src/building_blocks.kt
@@ -0,0 +1,10 @@
+fun main(args: Array<String>) {
+    val x: Int = 10
+    val arr: Array<Int> = arrayOf(10,20,30,40,50,60)
+    val arrX: Array<Int> = arrayOf(10,20,30,40,50,60)
+    val arr2: ArrayList<Int> = arrayListOf(1,2,3,4,5,6)
+    val arr20: ArrayList<Int> = arrayListOf(1,2,3,4,5,6)
+    val arr3: Array<String> = arrayOf("String1", "String2", "String3")
+    var new = arr2 + arr2
+    println(new)
+}
